c4316676a52447e209784f93f5932e1fec07170c
ShaikNehaJabeen <shaikjabeen573@gmail.com>
2025-09-16 21:25:53 +0530

    Update Cart.jsx

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -3,7 +3,7 @@ import { useCart } from "../context/CartContext";
 import { useNavigate } from "react-router-dom";
 import "./style.css";
 
-const BASE_URL = 'http://65.0.169.144:8083/back1';
+const BASE_URL = 'http://44.220.161.28:8082/ecommerce';
 
 const Cart = () => {
   const { cartItems, removeFromCart } = useCart();
