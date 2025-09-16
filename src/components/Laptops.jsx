d1d4727a8f4a9537cb3d8da3fbc3d91cb89629a4
ShaikNehaJabeen <shaikjabeen573@gmail.com>
2025-09-16 21:25:09 +0530

    Update Laptops.jsx

--- src/components/Laptops.jsx
+++ src/components/Laptops.jsx
@@ -4,7 +4,7 @@ import { getProducts } from "../services/productService";
 import { useNavigate } from "react-router-dom";
 import "./style.css";
 
-const BASE_URL = 'http://65.0.169.144:8083/back1';
+const BASE_URL = 'http://44.220.161.28:8082/ecommerce';
 
 const Laptops = () => {
   const [products, setProducts] = useState([]);
